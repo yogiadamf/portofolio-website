8ef4849bd5f3814891e4f2c34be73d9ae73daa66
Yogi Adam Firdaus <yogiadamf@gmail.com>
2022-03-08 16:46:24 +0700

    Google Analaytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ import { AnimatePresence } from 'framer-motion'
 import Chakra from '../components/chakra'
 import './Resume/Resume.css'
 import './Contact/ContactMe.css'
-import Script from 'next/script'
 
 if (typeof window !== 'undefined') {
   window.history.scrollRestoration = 'manual'
@@ -14,17 +13,6 @@ if (typeof window !== 'undefined') {
 function Website({ Component, pageProps, router }) {
   return (
     <>
-    <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}://www.googletagmanager.com/gtag/js?id=G-X1Z2S1J58Z`}
-      />
-      <Script strategy="lazyOnload">
-        {`window.dataLayer = window.dataLayer || []; 
-          function gtag(){dataLayer.push(arguments);} 
-          gtag('js', new Date()); 
-          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});  
-        `}
-      </Script>
       <Chakra cookies={pageProps.cookies}>
         <Fonts />
         <Layout router={router}>
